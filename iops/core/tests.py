2f6d50dc334f54336881c10939f92efc9745f7ac
Luan Teylo <luan.teylo@inria.fr>
2024-05-02 11:39:33 +0200

    adding the test class

---
+++ iops/core/tests.py
@@ -0,0 +1,194 @@
+
+from iops.core.config import IOPSConfig
+from iops.util.generator import Generator
+from iops.util.tags import jobManager, TestType, Pattern, Operation
+
+from pathlib import Path
+from abc import ABC, abstractmethod
+
+
+from abc import ABC, abstractmethod
+
+class Test(ABC):
+    """
+    Represents a test to be executed in the IOPS benchmark. 
+    This class serves as a base class for creating test scenarios with various configurations and behaviors. 
+    It is designed to be subclassed, with subclasses needing to process specific test parameters and implement the method to build necessary files.
+    """
+    _id_counter = 0
+
+    def __init__(self, config: 'IOPSConfig', round_path: Path, test_parameters: dict):
+        """
+        Initializes a new test instance, setting up essential file paths and storing configuration details.
+        
+        :param config: Configuration object containing settings for I/O operations.
+        :param round_path: Base path where test files will be located.
+        :param test_parameters: Dictionary of parameters that define test specifics. These parameters need to be handled appropriately by any class that inherits from this class.
+        """
+        type(self)._id_counter += 1
+        self.test_id = self._id_counter
+        
+        self.config = config
+        self.round_path = round_path
+        self.test_parameters = test_parameters
+
+        self.batch_path = round_path / f"test_{self.test_id}"
+        self.summary_file = self.batch_path / f"summary_test_{self.test_id}_$(date +%Y%m%d%H%M%S)_$RANDOM.out"
+        self.batch_file = self.batch_path / f"batch_{self.test_id}.sh"
+
+    @classmethod
+    def create_test(cls, pattern: Pattern, operation: Operation,  config: 'IOPSConfig', round_path: Path, test_parameters: dict) -> 'Test':
+        """
+        Factory method to create test instances based on the test pattern and operation.
+        
+        :param pattern: The I/O pattern for the test.
+        :param operation: The type of operation to be performed.
+        :param config: Configuration object for the test.
+        :param round_path: Path where test files will be located.
+        :param test_parameters: Dictionary of specific test parameters.
+        :return: An instance of a subclass of Test.
+        """
+        if pattern == Pattern.SEQUENTIAL and operation == Operation.WRITE:            
+            return TestIOR(config, round_path, test_parameters)        
+        # Add more elif statements for other test types as needed
+        else:
+            raise ValueError(f"Unsupported test: {pattern} {operation}")
+
+    @abstractmethod
+    def build_files(self) -> None:
+        """
+        Abstract method that must be implemented by subclasses to generate the files needed to execute the test.
+        Subclasses should provide their own implementation to handle file generation based on the test configuration and parameters.
+        """
+        pass
+
+    def template(self):
+        if self.config.job_manager == jobManager.SLURM:
+            return self.config.slurm_template   
+        elif self.config.job_manager == jobManager.LOCAL:
+            return self.config.local_template
+        else:
+            return None     
+    
+
+
+    @classmethod
+    def from_existing(cls, existing_test: 'Test'):
+        """
+        Creates a new instance based on an existing test instance. This method dynamically determines the class of the existing test and creates a new instance of that class.
+
+        :param existing_test: An instance of a subclass of Test from which to create a new instance.
+        """
+        if isinstance(existing_test, Test):
+            # Dynamically get the class of the existing test and use it to create a new instance.
+            existing_class = type(existing_test)
+            return existing_class(config=existing_test.config, 
+                                  round_path=existing_test.round_path, 
+                                  test_parameters=existing_test.test_parameters)
+        else:
+            raise TypeError(f"Cannot create a new instance from the given test object. It must be a non-abstract subclass of Test.")
+
+
+
+    def __repr__(self):
+        msg_str = f"\t{self.test_id:04}: "
+        for key, value in self.test_parameters.items():            
+            msg_str += f"\[{key}={value}] "
+        
+        return msg_str
+
+
+
+class TestIOR(Test):
+    """
+    I/O pattern: sequential write in a single shared file.
+    Parameters:
+    - volume: The amount of data (in MB) involved in the test.
+    - folder_index: The index of the directory where the test files will be placed (each directory represents a different striping setup)
+    - computing: The number of computing nodes involved in the I/O operation.
+    """
+
+    def __init__(self, config: 'IOPSConfig', round_path: Path, test_parameters: dict):
+        # Initialize base class with shared configurations and the round path
+        super().__init__(config, round_path, test_parameters)
+        
+    
+    def __get_ior_command(self, delete_generate_file: bool) -> str:
+        """
+        Generate the IOR command based on the parameters defined in TestIOR.
+        """
+        
+        ior_command : str = "ior"
+
+        block_size : int = self.volume / (self.config.processes_per_node * self.computing)
+       
+        # Add parameters to the command
+
+        ior_command += f" -w" # only write operation for now
+        ior_command += f" -t 1m"  
+        ior_command += f" -b {int(block_size)}m"  
+
+        # delete the generate file at the end
+        if not delete_generate_file:
+            ior_command += f" -k" 
+        ior_command += f" -O summaryFile={self.summary_file}" # Path where the output will be written 
+        ior_command += f" -O summaryFormat=default"
+        # Path where the output will be written 
+        ior_command += f" -o {self.config.get_stripe_folder(self.folder_index)}/test{self.test_id}.ior"
+        
+        return ior_command
+
+    def build_files(self) -> None:
+        """
+        Generate a batch file capable of executing the IOR command generated by the previous method.
+        """
+        # First create the folder for the test
+        self.batch_path.mkdir(parents=True, exist_ok=True)
+
+        parameters = {}
+        if self.config.modules is not None:
+            parameters["modules"] = self.config.modules
+    
+        if self.config.slurm_constraint is not None:
+            parameters["constraint"] = self.config.slurm_constraint
+        
+        if self.config.slurm_partition is not None:
+            parameters["partition"] = self.config.slurm_partition
+        
+        if self.config.slurm_time is not None:
+            parameters["time"] = self.config.slurm_time
+        else:
+            parameters["time"] = "04:00:00"
+        
+        parameters["job_name"] = f"iops_{self.test_id}"
+        parameters["chdir"] = self.batch_file.parent
+        parameters["ntasks"] = self.computing * self.config.processes_per_node
+        parameters["nodes"] = self.computing
+        parameters["ntasks_per_node"] = self.config.processes_per_node
+        parameters["benchmark_cmd"] = self.__get_ior_command(True)
+        
+        # Generate the execution script
+        Generator.from_template(template_path=self.template(), output_path=self.batch_file, info=parameters)
+        
+    @property
+    def volume(self) -> int:
+        """
+        The volume of data involved in the test, in MB.
+        """
+        return self.test_parameters["volume"]
+    
+    @property
+    def folder_index(self) -> int:
+        """
+        The index of the directory within the storage hierarchy where the test files will be placed.
+        """
+        return self.test_parameters["folder_index"]
+    
+    @property
+    def computing(self) -> int:
+        """
+        The number of computing nodes or processes dedicated to this test.
+        """
+        return self.test_parameters["computing"]
+
+    
