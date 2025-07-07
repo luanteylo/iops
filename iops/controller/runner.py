f9dd92a0d6f461c98024fe5ac07724fbf9419959
Luan Teylo <luan.teylo@inria.fr>
2025-07-07 11:44:15 +0200

    **Update runner by adding build functions**
    * The runner now calls the `build` functions for both the benchmark and executor classes. This allows selecting the appropriate class based on the config file.
    * Finalized and closed the loop for analyzer integration. The new phase is now built based on the statistics defined in the analyzer.
    * Full results and history are saved at the end of each phase. A CSV file is generated per phase, summarizing all executions.

--- iops/controller/runner.py
+++ iops/controller/runner.py
@@ -1,6 +1,6 @@
-from iops.controller.executors import SlurmExecutor, LocalExecutor
+from iops.controller.executors import SlurmExecutor, LocalExecutor, BaseExecutor
 from iops.analysis.metrics import MetricsAnalyzer
-from iops.benchmarks.ior import IORBenchmark
+from iops.benchmarks.ior import IORBenchmark, BenchmarkRunner
 from iops.utils.logger import HasLogger
 from iops.controller.planner import SweepPlanner
 from iops.controller.planner import PhaseResult
@@ -8,18 +8,22 @@ from iops.utils.config_loader import IOPSConfig
 from iops.utils.file_utils import FileUtils 
 
 class IOPSRunner(HasLogger):
-    def __init__(self, config):
+    def __init__(self, config: IOPSConfig):
         super().__init__()
         self.config = config
 
     def run(self):
         self.logger.info("Starting IOPS benchmarking process")
 
-        benchmark = IORBenchmark(self.config)
+        benchmark = BenchmarkRunner.build(name=self.config.execution.benchmark_tool, 
+                                          config=self.config)
+        
+        executor = BaseExecutor.build(name=self.config.execution.job_manager, 
+                                      config=self.config)
+    
         planner = SweepPlanner(self.config, benchmark)
-        analyzer = MetricsAnalyzer()
-        executor = SlurmExecutor(self.config)  # or LocalExecutor depending on config
-        # executor = LocalExecutor(self.config)  # For local testing, replace with SlurmExecutor for actual cluster runs
+        analyzer = MetricsAnalyzer()        
+        
                 
         while planner.has_next_phase():
             phase = planner.next_phase()
@@ -36,25 +40,32 @@ class IOPSRunner(HasLogger):
                 try:
                     job_script = benchmark.generate(params=params)
                     job_id = executor.submit(job_script)  # Replace with actual job submission logic
-                    output_data = executor.wait_and_collect(job_id)                    
-                    # result = benchmark.parse_output(output_data["output_path"])
+                    output_data = executor.wait_and_collect(job_id)      
+                    self.logger.debug(f"Job {job_id} completed. Output data: {output_data}")              
 
-                    #self.logger.info(f"\tBandwidth: {result.get('bandwidth', 'N/A')} MB/s, Latency: {result.get('latency', 'N/A')} ms")
+                    # check if output_data is valid
+                    if output_data.get('status') == 'completed':
+                        # call parse_output to extract metrics
+                        result = benchmark.parse_output(params=params)
+                    
+                    self.logger.debug(f"Parsed result: {result}")
 
-                    #analyzer.record(result, params)
+                    analyzer.record(result, params)
                     #last_result = {"params": params, "result": result}
-                    self.logger.info(f"Simulating test execution for parameters: {params}")
+                    #self.logger.info(f"Simulating test execution for parameters: {params}")
 
                 except Exception as e:
                     self.logger.error(f"Error during test execution: {e}")
                     raise
 
-            #best = analyzer.select_best(phase.criterion)
-            #self.logger.info(f"Best parameters for phase '{phase.sweep_param}':")
-            #self.logger.info(f"\tnodes: {best.get('nodes')}, volume: {best.get('volume')}, ost_count: {best.get('ost_count').name}")
-            #self.logger.info(f"\tBest {phase.criterion}: {best.get(phase.criterion)}")
+            best = analyzer.select_best(phase.criterion)
+            self.logger.info(f"Best parameters for phase '{phase.sweep_param}':")
+            self.logger.info(f"\tnodes: {best.get('nodes')}, volume: {best.get('volume')}, ost_count: {best.get('ost_count').name}")
+            self.logger.info(f"\tBest {phase.criterion}: {best.get(phase.criterion)}")
+            analyzer.save_record_csv(planner.current_phase_path / f"results_{phase.sweep_param}.")
 
-            #planner.update_for_next_phase(PhaseResult(phase.sweep_param, best))
-            #analyzer.clean()
+            planner.update_for_next_phase(PhaseResult(phase.sweep_param, best))
+
+            analyzer.clean()
         
         self.logger.info("All benchmarking phases completed.")
